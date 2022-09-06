8580d302351665dafe0953252aae7e05964c834f
LissetteQ <lissetteq123@gmail.com>
2022-09-06 09:46:34 -0300

    cambios en el feed.js

--- src/views/feed.js
+++ src/views/feed.js
@@ -1,7 +1,12 @@
 import { loginRoot } from "../main.js";
 
 export function feedView() {
-  const titleFeed = document.createElement("h1");
-  titleFeed.textContent = "ESTE ES EL MURO";
-  root.appendChild(titleFeed);
+  const containFeed = document.createElement("div");
+  containFeed.setAttribute("class", "containFeed");
+  root.appendChild(containFeed);
+
+  const logoFeed = document.createElement("img");
+  logoFeed.src = "./img/logoo.jpg";
+  logoFeed.setAttribute("class", "logoFeed");
+  containFeed.appendChild(logoFeed);
 }
--- src/views/login.js
+++ src/views/login.js
@@ -1,19 +1,3 @@
-// export default () => {
-//   const login = `
-//     <div id="logo"><img src="./img/logoo.jpg" alt="logo-header"></div>
-//     <h1>Iniciar sesión</h1>
-//     <div class = "login">
-//     <input type="email" placeholder="Ingresa tu correo">
-//     <input type="password" placeholder="Ingresa tu contraseña">
-//     <button id ="submit"> <a href="#/submit">Conectar</a></li></button>
-//     <button id ="register"> <a href="#/register">Registrarse</a></li></button>
-//     </div>
-//     `;
-
-//   const divElement = document.createElement("div");
-//   divElement.innerHTML = login;
-//   return divElement;
-// };
 import { loginRoot } from "../main.js";
 
 export function loginView() {
